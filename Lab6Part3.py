329890bca69392260dbd681079895ce7fa78cb00
KenyaDacoulis <kenyadacoulis@gmail.com>
2025-10-23 09:40:39 -0400

    Update Lab6Part3.py
    first 5 parts of part 3

--- Lab6Part3.py
+++ Lab6Part3.py
@@ -5,3 +5,25 @@ Created on Thu Oct 23 08:22:17 2025
 @author: Kenya Dacoulis
 """
 
+import pandas as pd
+import seaborn as sns
+
+data = pd.read_csv('wdi_wide.csv')
+
+#Part 3.3
+info = data.info()
+
+# number of empty values physicians: 217-207 = 10; empty values
+# number of empty values populations: 217-217 = 0; no empty values
+
+#Part 3.4
+
+unique_values = data.nunique()
+print(unique_values)
+
+#Part 3.5
+
+further_info = data.describe ()
+print(further_info)
+
+
