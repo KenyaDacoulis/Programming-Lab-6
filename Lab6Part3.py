bc5a966c23c844ffb9ccd6948dbd819e4b4220e7
Carlynmatar <carlynmatar@gmail.com>
2025-10-24 12:09:39 -0400

    Update Lab6Part3.py
    Finished part 3, started part 4

--- Lab6Part3.py
+++ Lab6Part3.py
@@ -2,10 +2,12 @@
 """
 Created on Thu Oct 23 08:22:17 2025
 
+@author: Kenya Dacoulis and Carlyn Matar
 """
 
+#Part 3 
+
 import pandas as pd
-import seaborn as sns
 
 data = pd.read_csv('wdi_wide.csv')
 
@@ -22,5 +24,43 @@ print(unique_values)
 
 #Part 3.5
 
-further_info = data.describe ()
+further_info = data.describe()
 print(further_info)
+
+# gives count, mean, standard deviation, minimum, max, 
+
+#Part 3.6
+
+data['GNI per capita'] = round(data['GNI']/ data['Population'])
+
+# Part 3.7
+
+country_count_per_region = pd.value_counts(data['Region'])
+
+high_income_country_count = pd.value_counts(data['High Income Economy'])
+
+# Part 3.8
+
+high_income_location = pd.crosstab(data['High Income Economy'], data['Region'])
+print(high_income_location)
+
+# Part 3.9
+
+filtered_data = data[data['Life expectancy, female'] > 80]
+
+for i in filtered_data["Country Name"]:
+    print(i)
+    
+
+#Part 4 
+import seaborn as sns
+
+#Part 4.1 and 4.2
+sns.relplot(x=data['Life expectancy, female'], y=data['GNI per capita'], hue= data['Region'])
+sns.relplot(x=data['Life expectancy, male'], y=data['GNI per capita'], hue= data['Region'])
+#yes, we inverted the y and x axis to better show the curve, but essentially, as the GNI per capita increases, the life expectancy also increases. 
+
+#Part 4.3
+sns.relplot(x=data['Life expectancy, female'], y=data['GNI per capita'], hue= data['Region'], kind='line', errorbar = 'sd')
+sns.relplot(x=data['Life expectancy, male'], y=data['GNI per capita'], hue= data['Region'], kind='line', errorbar = 'sd')
+#The standard deviation doesn't show since it's always 0, as each colum represents the data from a single country, so theres only one trial
